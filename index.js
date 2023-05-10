ef04bf028b65fa77942de422eb50b87090524d99
antoni-devlin <antoni.devlin@gmail.com>
2023-05-10 18:56:05 +0100

    Working fizzbuzz

--- index.js
+++ index.js
@@ -1,10 +1,18 @@
-// If a number n is not divisible by 3 or 5, return n
+// If n is divisible by 3 and 5, return fizzbuzz
 // If n is divisible by 3 and not by 5, return fizz
 // If n is divisible by 5 and not 3, return buzz
-// If n is divisible by 3 and 5, return fizzbuzz
+// If n is not divisible by 3 or 5, return n
+
 export function fizzbuzz(n) {
-  if (n === 3) {
-    return "fizz";
+  if (isNaN(n)) {
+    throw new TypeError(`${n} isn't a number.`);
   }
-  return 1;
+  if (n % 3 === 0 && n % 5 === 0) {
+    return "fizzbuzz";
+  } else if (n % 3 === 0) {
+    return "fizz";
+  } else if (n % 5 === 0) {
+    return "buzz";
+  }
+  return n;
 }
