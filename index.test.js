9780481e46aa039f1d93646fc8e1a03378eecadc
antoni-devlin <antoni.devlin@gmail.com>
2023-05-10 18:56:34 +0100

    Fizzbuzz tests. Check isNaN test fails...

--- index.test.js
+++ index.test.js
@@ -18,9 +18,27 @@ describe("functions", () => {
     expect(result).toBe("fizz");
   });
 
-  test("fizzbuzz(3) is fizz", () => {
-    const result = fizzbuzz(3);
+  test("fizzbuzz(5) is buzz", () => {
+    const result = fizzbuzz(5);
 
-    expect(result).toBe("fizz");
+    expect(result).toBe("buzz");
+  });
+
+  test("fizzbuzz(45) is fizzbuzz", () => {
+    const result = fizzbuzz(45);
+
+    expect(result).toBe("fizzbuzz");
+  });
+
+  test("fizzbuzz(7) is fizzbuzz", () => {
+    const result = fizzbuzz(7);
+
+    expect(result).toBe(7);
+  });
+
+  test('fizzbuzz("string") throws a TypeError', () => {
+    const result = fizzbuzz("string");
+
+    expect(result).toThrow("/isn't a number/");
   });
 });
